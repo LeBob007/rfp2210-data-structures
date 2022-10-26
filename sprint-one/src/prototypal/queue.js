3e02bbe7c3673c1d9d313a324e033c32c5a1ddfa
Robert Hu <robert.y.hu.97@gmail.com>
2022-10-26 12:35:20 -0700

    Prototypal finished

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+  queue.storage = {};
+  queue.start = 0;
+  queue.index = 0;
+  return queue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.index] = value;
+  this.index++;
+};
 
+queueMethods.dequeue = function() {
+  if (this.start < this.index) {
+    var temp = this.storage[this.start];
+    delete this.storage[this.start];
+    this.start++;
+    return temp;
+  }
+};
+
+queueMethods.size = function() {
+  return this.index - this.start;
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+  stack.storage = {};
+  stack.index = 0;
+  return stack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.index] = value;
+  this.index++;
+};
 
+stackMethods.pop = function() {
+  this.index--;
+  if (this.index < 0) {
+    this.index = 0;
+  }
+  return this.storage[this.index];
+};
+
+stackMethods.size = function() {
+  return this.index;
+};
