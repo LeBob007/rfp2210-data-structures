f3c7b4efe747b185c2a132aa85abaeb057d76edf
Robert Hu <robert.y.hu.97@gmail.com>
2022-10-26 12:20:13 -0700

    Functional and Functional-shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {};
+  stack.storage = {};
+  stack.index = 0;
+  _.extend(stack, stackMethods);
+  return stack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.index] = value;
+  this.index++;
+};
 
+stackMethods.pop = function() {
+  this.index--;
+  if (this.index < 0) {
+    this.index = 0;
+  }
+  return this.storage[this.index];
+};
+
+stackMethods.size = function() {
+  return this.index;
+};
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,26 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var index = 0;
+  var start = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[index] = value;
+    index++;
   };
 
   someInstance.dequeue = function() {
+    if (start < index) {
+      var temp = storage[start];
+      delete storage[start];
+      start++;
+      return temp;
+    }
   };
 
   someInstance.size = function() {
+    return index - start;
   };
 
   return someInstance;
